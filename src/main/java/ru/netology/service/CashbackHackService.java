b5053f647f7c00d9fac8e623be8d4f9d45836da1
Nikolay Gruzdev <gruzdevni@mail.ru>
2019-10-22 22:00:36 +0300

    Service updated to V2

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -4,6 +4,16 @@ public class CashbackHackService {
     private static final int boundary = 1000;
 
     public static int remain(int amount) {
-        return boundary - amount % boundary;
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be greater than zero");
+        }
+
+        boolean isNeedMore = amount % boundary != 0;
+        if (!isNeedMore) {
+            return 0;
+        }
+
+        int remain = boundary - amount % boundary;
+        return remain;
     }
 }
