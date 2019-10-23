7f943b8cc72badc5da8acb5e4a776a08d2cfb155
Nikolay Gruzdev <gruzdevni@mail.ru>
2019-10-23 23:26:57 +0300

    Exception test fix, README update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 [![Build status](https://ci.appveyor.com/api/projects/status/txe1ydywcyieh426?svg=true)](https://ci.appveyor.com/project/gruzdevni/aqa2)
+
+# Test set for Cashback hack service
+### This test set is a Gradle project. It contains some JUnit sample tests:
+* Parameterized test for border values;
+* Test for exception.
+
+### To install it:
+1. Clone or download repository
+2. Open with IntelliJ IDEA or other IDE.
--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,19 +1,13 @@
 package ru.netology.service;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
-
-    @Test
-    void shouldReturn0IfAmount1000() {
-        int actual = CashbackHackService.remain(1000);
-        int expected = 0;
-        assertEquals(expected, actual);
-    }
 
     @ParameterizedTest
     @CsvFileSource(resources = "/DataForTests.csv", numLinesToSkip = 1)
@@ -24,12 +18,15 @@ class CashbackHackServiceTest {
 
     @Test
     void shouldThrowExceptionIfAmountLessThanZero() {
+        assertThrows(IllegalArgumentException.class, () -> CashbackHackService.remain(-100));
+
+        /*Usage of try-catch in tests is not good practice
         boolean thrown = false;
         try {
-            CashbackHackService.remain(-100);
+            ;
         } catch (IllegalArgumentException e) {
             thrown = true;
         }
-        assertTrue(thrown);
+        assertTrue(thrown);*/
     }
 }
